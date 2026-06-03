59fd71fb541d26a50a9588a7dea980336495d441
133jeff <jeffteh68@gmail.com>
2026-06-03 21:42:59 +0800

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-BOT_TOKEN = os.getenv("8822216767:AAF-TEbnyncD4pnys4lhu6IRPEyf74HuXwc")
-CHAT_ID = os.getenv("7694683964")
+set BOT_TOKEN=8822216767:AAF-TEbnyncD4pnys4lhu6IRPEyf74HuXwc
+set CHAT_ID=7694683964
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
